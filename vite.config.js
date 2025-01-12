6d91d9eea1c103ac08f5042e0166a65261da854b
unkabas <undercannabis10@gmail.com>
2025-01-12 09:18:06 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import { defineConfig } from 'vite'
 export default defineConfig({
 	plugins: [react()],
 	server: {
+		 host:true,
+    port:8000,
+    watch:{
+      usePolling:true
+
 		proxy: {
 			'/api': {
 				target: 'https://www.balldontlie.io',
